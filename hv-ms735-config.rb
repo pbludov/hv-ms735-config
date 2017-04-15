1277d3edb8813bfa9f3fbe5b2825c54f009c6f1f
Pavel Bludov <pbludov@gmail.com>
2017-04-15 23:35:05 +0800

    Updated SHA256.

--- hv-ms735-config.rb
+++ hv-ms735-config.rb
@@ -1,6 +1,6 @@
 cask 'hv-ms735-config' do
   version '1.0.1'
-  sha256 '61f18eaea955cc0fd928666b7e9ea8289b4f52ad22da45f3f88ecae7fb0493af'
+  sha256 '56a52eee0d52ecbb562700b00c4d75579252eb0c35d19c85b53b98a9a5620247'
   url "https://github.com/pbludov/hv-ms735-config/releases/download/v1.0.1/hv-ms735-config.dmg"
   name 'HV MS735'
   homepage 'https://github.com/pbludov/hv-ms735-config/'

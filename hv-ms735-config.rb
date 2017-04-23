7f91c2eee214e0b421b465bdc3c3eafb37870747
Pavel Bludov <pbludov@gmail.com>
2017-04-23 10:57:11 +0800

    Choco update

--- hv-ms735-config.rb
+++ hv-ms735-config.rb
@@ -1,6 +1,6 @@
 cask 'hv-ms735-config' do
   version '1.1.0'
-  sha256 'a8ae7ba7b50410a107b4cc826662378ab8b6d79014b8ac3512ebae2505c80098'
+  sha256 '03e5466ca0c3b0044fc0f76cfb1fd92d419256e526644db4b19c76a5ba377e27'
   url "https://github.com/pbludov/hv-ms735-config/releases/download/v1.1.0/hv-ms735-config.dmg"
   name 'HV MS735'
   homepage 'https://github.com/pbludov/hv-ms735-config/'
